14c1fc84e45fb86900c0bc46f331a33666acc915
Alex Rodrigues <alex.adaumi@gmail.com>
2022-11-12 14:46:33 -0300

    feat: electron inittial structure

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# Tudum Desktop
+
+Tudum is a simple, easy to use, and beautiful to look at task manager.
---
+++ public/electron.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    resizable: false,
+    maximizable: false,
+  });
+
+  win.removeMenu();
+
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
